bf7dc8cd0e882f0ee1f0bcec016b342c2bff120c
Lorenzo-Ruben-Rampa <lorenzo.rampa.ruben@gmail.com>
2025-06-05 23:24:55 +0200

    bonus

--- src/main/java/org/lessons/java/spring_intro/controller/HomeController.java
+++ src/main/java/org/lessons/java/spring_intro/controller/HomeController.java
@@ -2,14 +2,41 @@ package org.lessons.java.spring_intro.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    //Metodo home
+    //Metodo rotta home
     @GetMapping
     public String home() {
-        return "home"; // corrisponde a src/main/resources/templates/home.html
+        return "home"; 
+    }
+
+    @GetMapping("greetings")
+    //Metodo rotta greetings
+    public String greetings(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("username", "Skylor");
+        model.addAttribute("name", name);
+        model.addAttribute("email", "lorenzo.rampa.ruben@hotmail.com");
+        model.addAttribute("currentDate", LocalDate.now());
+        model.addAttribute("currentTime", LocalTime.now());
+        return "greetings"; 
     }
     
+    @GetMapping("country")
+    //Metodo rotta country
+    public String country(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("username", "Skylor");
+        model.addAttribute("name", name);
+        model.addAttribute("countryName", "Italia");
+        model.addAttribute("currentDate", LocalDate.now());
+        model.addAttribute("currentTime", LocalTime.now());
+        return "country";
+    }
+
 }
